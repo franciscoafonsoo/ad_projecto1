d7aa3248e4b99f7d12692f6cac0fd99d51429aa9
sherby <sherby@bitbucket.org>
2016-02-24 17:55:04 +0000

    cleanup

--- lock_client.py
+++ lock_client.py
@@ -7,7 +7,26 @@ Números de aluno:
 """
 # Zona para fazer imports
 
-
+import sys,sock_utils
 
 # Programa principal
+
+if len(sys.argv) > 2:
+    HOST = sys.argv[1]
+    PORT = int(sys.argv[2])
+
+    while True:
+
+        msg = raw_input("Comando: ")
+        client_sock = sock_utils.create_tcp_client_socket(HOST, PORT)
+        if msg == "exit":
+            sys.exit()
+        client_sock.sendall(msg)
+        resposta = sock_utils.receive_all(client_sock, 1024)
+        print 'Recebi ', resposta
+        client_sock.close()
+
+else:
+    print "Sem argumentos ou argumentos incompletos"
+
 
